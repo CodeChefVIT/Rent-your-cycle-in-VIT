83e9051da2516bd8debfd67cb38cb5de3c135838
SayarB <sayarbhattacharyya03@gmail.com>
2022-07-22 21:02:10 +0530

    feat: login page added

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -1,22 +1,23 @@
-import React from "react";
-import { ButtonBase } from "@mui/material";
-function CustomButton({ children, bgColor }) {
+import React from 'react'
+import { ButtonBase } from '@mui/material'
+function CustomButton ({ children, bgColor, onClick }) {
   return (
     <ButtonBase
+      onClick={onClick}
       sx={{
-        borderRadius: "20px",
-        padding: "10px 0",
-        width: "140px",
-        border: "none",
+        borderRadius: '20px',
+        padding: '10px 0',
+        width: '140px',
+        border: 'none',
         backgroundColor: bgColor,
-        margin: "10px",
-        fontSize: "2rem",
-        fontWeight: "100px",
+        margin: '10px',
+        fontSize: '2rem',
+        fontWeight: '100px'
       }}
     >
       {children}
     </ButtonBase>
-  );
+  )
 }
 
-export default CustomButton;
+export default CustomButton
--- frontend/src/pages/HomePage/Home.js
+++ frontend/src/pages/HomePage/Home.js
@@ -1,25 +1,35 @@
-import React, { useState } from "react";
-import logo from "../../logo.svg";
-import "./Home.css";
-import Button from "../../components/Button/Button";
-function Home() {
-  const [userAuthenticated] = useState(false);
-  if (!userAuthenticated)
+import React, { useState } from 'react'
+import logo from '../../logo.svg'
+import './Home.css'
+import Button from '../../components/Button/Button'
+import { useNavigate } from 'react-router-dom'
+function Home () {
+  const [userAuthenticated] = useState(false)
+  const navigate = useNavigate()
+  if (!userAuthenticated) {
     return (
-      <div className="home_page">
-        <div className="container">
-          <img src={logo} alt="" className="logo_image" />
-          <Button bgColor={"#DEE2E6"}>Login</Button>
-          <Button bgColor={"#DEE2E6"}>Sign Up</Button>
+      <div className='home_page'>
+        <div className='container'>
+          <img src={logo} alt='' className='logo_image' />
+          <Button
+            onClick={() => {
+              navigate('/login')
+            }}
+            bgColor='#DEE2E6'
+          >
+            Login
+          </Button>
+          <Button bgColor='#DEE2E6'>Sign Up</Button>
         </div>
       </div>
-    );
-  else
+    )
+  } else {
     return (
       <div>
         <h1>Authenticated</h1>
       </div>
-    );
+    )
+  }
 }
 
-export default Home;
+export default Home
