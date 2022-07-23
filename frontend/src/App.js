ec4e5be4b471c9a197e867e1d855ef0ee7178b27
SayarB <sayarbhattacharyya03@gmail.com>
2022-07-23 15:02:43 +0530

    fix: merge conflict resolved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg'
-import Home from './pages/HomePage/Home'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './pages/LoginPage/Login'
-function App () {
+import logo from "./logo.svg";
+import Home from "./pages/HomePage/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/LoginPage/Login";
+import SignUp from "./pages/SignUpPage/SignUp";
+function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Router>
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/login' element={<Login />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<SignUp />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
