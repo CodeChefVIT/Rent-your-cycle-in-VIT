fa7d622d16745fe291597f9e08c4a0d439a78b34
SayarB <sayarbhattacharyya03@gmail.com>
2022-07-25 13:40:32 +0530

    feat: bike register page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import logo from "./logo.svg";
 import Home from "./pages/HomePage/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/LoginPage/Login";
 import SignUp from "./pages/SignUpPage/SignUp";
+import BikeRegister from "./pages/BikeRegister/BikeRegister";
 function App() {
   return (
     <div className="app">
@@ -11,6 +11,7 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<SignUp />} />
+          <Route exact path="/bike/register" element={<BikeRegister />} />
         </Routes>
       </Router>
     </div>
