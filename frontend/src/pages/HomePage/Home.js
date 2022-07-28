abab4c8951cec1b055d30a867032e069bcc4e4dc
SayarB <sayarbhattacharyya03@gmail.com>
2022-07-28 19:24:41 +0530

    feat: log out button on home page

--- frontend/src/pages/HomePage/Home.js
+++ frontend/src/pages/HomePage/Home.js
@@ -3,11 +3,25 @@ import logo from "../../logo.svg";
 import "./Home.css";
 import Button from "../../components/Button/Button";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { AuthSelector, userSelector } from "../../reducers/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  AuthSelector,
+  userSelector,
+  set_user,
+  remove_user,
+} from "../../reducers/userSlice";
+
 function Home() {
   const { isAuth } = useSelector(userSelector);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const userLogOut = ()=>
+  {
+    dispatch(remove_user());
+    
+  }
+
   if (!isAuth) {
     return (
       <div className="home_page">
@@ -36,6 +50,9 @@ function Home() {
     return (
       <div>
         <h1>Authenticated</h1>
+        <Button onClick={userLogOut} bgColor={"#eeeeee"}>
+          Log Out
+        </Button>
       </div>
     );
   }
